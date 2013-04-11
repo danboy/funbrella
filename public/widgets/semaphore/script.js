bb1052faf170f5cc4c7b1c6cfbee2aba4854d9b9
Dan Nawara <dan@pinmonkey.com>
2013-04-10 20:26:13 -0500

    called out semaphore ring more.

--- public/widgets/semaphore/script.js
+++ public/widgets/semaphore/script.js
@@ -46,7 +46,7 @@ Funbrella.semaphore.prototype = {
     return this.options.urls[this.url];
   }
 
-, template: Hogan.compile('<h1><a name="ring" class="{{result}} ring"/>{{project_name}}: <strong class="{{result}}">{{branch_name}}</strong></h1>{{#commit}}<h3>{{author_name}}</h3><p>{{message}}</p>{{/commit}}')
+, template: Hogan.compile('<a name="ring" class="{{result}} ring"/><h1>{{project_name}}: <strong class="{{result}}">{{branch_name}}</strong></h1>{{#commit}}<h3>{{author_name}}</h3><p>{{message}}</p>{{/commit}}')
 
 , render: function(data, self){
     var template = self.template.render(data);
