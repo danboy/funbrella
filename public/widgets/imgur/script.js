26e8c6addf239916d7007ab88b56328bcfc8d9f6
Dan Nawara <dan@pinmonkey.com>
2013-06-01 17:09:52 -0500

    updates some widgets, tweaks xml2json settings

---
+++ public/widgets/imgur/script.js
@@ -0,0 +1,21 @@
+var Funbrella = Funbrella || {};
+
+Funbrella.imgur = Funbrella.WidgetView.extend({
+  prefs: {
+    frequency: 60
+  , showNsfw: false
+
+  },
+  template: Hogan.compile('<div class="imgur"><h1>{{title}}</h1><img src="http://i.imgur.com/{{hash}}{{ext}}" class="{{scale}}" /></div>')
+, setup: function(){
+    this.url = "http://imgur.com/gallery/top/all.json"
+  }
+, data: function(data, cb){
+    var img = this.random(data.data);
+    if(img.nsfw && !this.prefs.showNsfw){
+      this.data(data)
+    }else{
+      cb(img);
+    }
+  }
+});
