c0a3042a2f026134b7d234cc0acb20dd4196c888
Dan Nawara <dan@pinmonkey.com>
2013-06-07 15:03:15 -0500

    makes data clip a bit more generic

--- public/widgets/data_clip/script.js
+++ public/widgets/data_clip/script.js
@@ -7,11 +7,17 @@ Funbrella.dataClip = Funbrella.WidgetView.extend({
   , url: ""
   }
 , requires: ['title', 'url']
-, template: Hogan.compile('<h1>{{title}}</h1><h2>{{data}}</h2>')
+, template: Hogan.compile('<h1>{{title}}</h1><table>{{#d}}<tr><td>{{name}}</td><td>{{value}}</td></tr>{{/d}}</table>')
 , setup: function(){
     this.url = this.prefs.url;
   }
 , data: function(data, cb){
-    cb({title: this.prefs.title, data: data.values[0][0]})
+    var clip = [];
+    console.log(data);
+    data.fields.forEach(function(d,i){
+      clip.push({name: d, value: data.values[0][i]});
+    });
+    console.log(clip);
+    cb({title: this.prefs.title, data: data.values[0][0], d: clip})
   }
 });
