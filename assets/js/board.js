dff8bae7a6f2c08674d7e560e95c68c95ee690fc
Dan Nawara <dan@pinmonkey.com>
2013-10-28 16:17:43 -0500

    adding jade extensions to hopefully fix mincer

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,5 +1,8 @@
 //= require_tree ./lib
-//= require_tree ./templates
+//= require_tree ./templates/messageBoard.jade
+//= require_tree ./templates/messageTemplate.jade
+//= require_tree ./templates/prefs.jade
+//= require_tree ./templates/watcher.jade
 //= require_tree ./models
 //= require_tree ./views
 
