a8d609fe7043b5d2a84ffc384ff41de1543e822c
Dan Nawara <dan@pinmonkey.com>
2013-10-28 11:53:50 -0500

    updates config keys for nodejitsu

--- config/auth.js
+++ config/auth.js
@@ -1,10 +1,10 @@
 module.exports = {
   github: {
-    appId: 'c7a01728d665caf77eb5'
-  , appSecret: '2b59cb076d2bc9e82e005cff2de1eaf4c73b0c9f'
+    appId: '4bc983ea46559e46c5da'
+  , appSecret: '70283ed510bb3c1bcace1f09c5e6700cea8782bd'
   }
 , google: {
-    appId: '742520875094.apps.googleusercontent.com'
-  , appSecret: 'qZGzg9xPqr3GzRApXRYJ9Wcq'
+    appId: '164843463126.apps.googleusercontent.com'
+  , appSecret: 'IzOROJnSlruWYI3q2ThbLe4g'
   }
 };
