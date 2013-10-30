5bf462a22414e8f065ef5b9352ba925f31d6f60e
Dan Nawara <dan@pinmonkey.com>
2013-10-30 10:55:17 -0500

    config update

--- config/app.js
+++ config/app.js
@@ -1,6 +1,6 @@
 Config = {
   production: {
-    db: 'mongodb://funbrella:2,-96Of%De,1rhs@dharma.mongohq.com:10064/funbrella'
+    db: 'mongodb://funbrella:1H3RddW2$@dharma.mongohq.com:10064/funbrella'
   }
 , development: {
     db: 'mongodb://localhost/funbrella'
